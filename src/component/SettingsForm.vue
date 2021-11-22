093365420e6520e65b2a6fc1dedc4a7e369ac258
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-22 14:42:21 +0300

    стили главного экрана

--- src/component/SettingsForm.vue
+++ src/component/SettingsForm.vue
@@ -2,18 +2,19 @@
 form#settings-form(@submit.prevent="handleSubmit")
   h4 Настройки
   input#settings-time-constraint(type="range" min="1" max="10" step="1" v-model="timeConstraint")
-  .mb-1
-  label(for="time-constraint") Длительность {{timeConstraint}} минут
-  .mb-1
+  label(for="time-constraint") Длительность: {{timeConstraint}} минут
+  .mb-2
   input#settings-difficulty(type="range" min="1" max="5" step="1" v-model="difficulty")
-  .mb-1
-  label(for="difficulty") Сложность {{difficulty}}
+  label(for="difficulty") Сложность: {{difficulty}}
+  .mb-2
   .operators
     .operator(v-for="operator in operatorOptions")
       input(type="checkbox" :value="operator.operator" v-model="operators")
       .mr-1
       | {{operator.label}}
-  button(type="submit") Начать
+  .mb-3
+  .actions
+    button.button(type="submit") Начать
 </template>
 
 <script lang="ts">
@@ -78,8 +79,42 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+#settings-form {
+  display: flex;
+  flex-direction: column;
+}
+
 .operator {
   display: flex;
   flex-direction: row;
+}
+
+input[type="range"] {
+  width: 250px;
+  margin: 0;
+  margin-bottom: 4px;
+}
+
+.actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+}
+
+.button {
+  outline: none;
+  text-transform: uppercase;
+  margin: 0;
+  padding: 8px 16px;
+  font-family: arial;
+  color: #78909C;
+  border: none;
+  border-radius: 2px;
+  background: white;
+  cursor: pointer;
+  box-shadow: 0 1px 2px 1px #B0BEC5;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 </style>
