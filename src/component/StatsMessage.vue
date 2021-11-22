7d6054c900ee346e44780c1c261f0a7a29d07742
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-22 13:44:02 +0300

    потребление примеси обработки игровой статистики в компоненте вывода игровой статистики

--- src/component/StatsMessage.vue
+++ src/component/StatsMessage.vue
@@ -10,30 +10,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import { GameSession } from '../types/types'
+import gameStatsMixin from '../mixin/gameStatsMixin'
 
 export default defineComponent({
   name: 'StatsMessage',
-  computed: {
-    stats() {
-      return this.$store.state.stats;
-    },
-    consecutiveDays(): number {
-      return this.$store.getters.consecutiveDays === 0 ? 'первый' : this.$store.getters.consecutiveDays;
-    },
-    lastSession(): GameSession | null {
-      return this.$store.getters.lastSession;
-    },
-    correctAnswers(): number {
-      return this.lastSession?.rate.correct ?? 0;
-    },
-    totalAnswers(): number {
-      return this.lastSession?.rate.total ?? 0;
-    },
-    correctAnswerRate(): number {
-      return Math.floor((this.correctAnswers / this.totalAnswers) * 100);
-    }
-  }
+  mixins: [gameStatsMixin]
 })
 </script>
 
